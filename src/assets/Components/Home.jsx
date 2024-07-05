902b984492b8bcab0547663e263ce8000af5ba5b
Marlon <marzonhapacheco@gmail.com>
2024-07-05 19:38:18 -0300

    teste

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -7,6 +7,7 @@ export default function Home() {
       <article className={style.F}>
         <div className={style.Txt}>
           <h1>Sergio</h1>
+          
           <h3>Uma biblioteca virtual é uma plataforma online que disponibiliza uma ampla variedade de materiais de leitura de forma digital. Nesse ambiente, você encontrará livros, artigos científicos, teses, dissertações, relatórios técnicos e outros recursos acadêmicos. A grande vantagem é que esses materiais podem ser acessados de qualquer lugar, a qualquer momento, desde que haja conexão à internet1. Além disso, a biblioteca virtual oferece uma experiência de usuário intuitiva, permitindo que você explore e pesquise com facilidade, sem filas ou multas de atraso. É uma ferramenta essencial para estudantes, pesquisadores e entusiastas do conhecimento. </h3>
         </div>
       </article>
